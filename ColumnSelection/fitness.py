a9bedb7374f9960651a321024e089a109796d0e6
Shih Feng <noahleft@gmail.com>
2015-01-05 21:44:09 +0800

    add uniform cross over function

--- ColumnSelection/fitness.py
+++ ColumnSelection/fitness.py
@@ -9,3 +9,12 @@ def oneCutCrossOver(parentPair):
   cutPoint=randint(1,len(parentPair[0])-1) # 49 cut points
   return [parentPair[0][:cutPoint]+parentPair[1][cutPoint:], \
           parentPair[1][:cutPoint]+parentPair[0][cutPoint:]]
+
+from random import random
+
+def uniformCrossOver(parentPair):
+  pair=[ round(random()) for x in range(len(parentPair[0]))]
+  str1=list(map(lambda x:parentPair[pair[x]][x],range(len(pair))))
+  str2=list(map(lambda x:parentPair[::-1][pair[x]][x],range(len(pair))))
+  return [str1,str2]
+
